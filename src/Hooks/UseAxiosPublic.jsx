7f6f9c514402caa98490788d56d07f9bec50f40d
ForhadAhmed367423 <afforhadah909@gmail.com>
2024-01-02 04:57:53 +0600

    commiting after added vercel link

--- src/Hooks/UseAxiosPublic.jsx
+++ src/Hooks/UseAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://beatrix-server.vercel.app/",
 })
 
 const UseAxiosPublic = () => {
